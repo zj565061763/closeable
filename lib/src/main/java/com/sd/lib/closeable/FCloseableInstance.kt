28972b9c0cc31cf5ef0b95bd62b87d032113c86a
zjun <565061763@qq.com>
2023-11-23 13:20:14 +0800

    rename

--- lib/src/main/java/com/sd/lib/closeable/FCloseableInstance.kt
+++ lib/src/main/java/com/sd/lib/closeable/FCloseableInstance.kt
@@ -3,7 +3,7 @@ package com.sd.lib.closeable
 import java.util.concurrent.atomic.AtomicInteger
 
 object FCloseableInstance {
-    private val _holder: MutableMap<Class<out AutoCloseable>, MutableMap<Any, Pair<AutoCloseable, AtomicInteger>>> = hashMapOf()
+    private val _instanceHolder: MutableMap<Class<out AutoCloseable>, MutableMap<Any, Pair<AutoCloseable, AtomicInteger>>> = hashMapOf()
 
     inline fun <reified T : AutoCloseable> key(key: Any, noinline factory: () -> T): Holder<T> {
         return key(T::class.java, key, factory)
@@ -12,8 +12,8 @@ object FCloseableInstance {
     @JvmStatic
     fun <T : AutoCloseable> key(clazz: Class<T>, key: Any, factory: () -> T): Holder<T> {
         synchronized(this@FCloseableInstance) {
-            val map = _holder[clazz] ?: hashMapOf<Any, Pair<AutoCloseable, AtomicInteger>>().also {
-                _holder[clazz] = it
+            val map = _instanceHolder[clazz] ?: hashMapOf<Any, Pair<AutoCloseable, AtomicInteger>>().also {
+                _instanceHolder[clazz] = it
             }
             val pair = map[key] ?: (factory() to AtomicInteger(0)).also {
                 map[key] = it
@@ -27,14 +27,14 @@ object FCloseableInstance {
 
     private fun <T : AutoCloseable> decrementCount(clazz: Class<T>, key: Any) {
         synchronized(this@FCloseableInstance) {
-            val map = checkNotNull(_holder[clazz])
+            val map = checkNotNull(_instanceHolder[clazz])
             val pair = checkNotNull(map[key])
             val count = pair.second.decrementAndGet()
             if (count > 0) {
                 null
             } else {
                 map.remove(key)
-                if (map.isEmpty()) _holder.remove(clazz)
+                if (map.isEmpty()) _instanceHolder.remove(clazz)
                 pair.first
             }
         }?.close()
