02695584bdbf5a059f63299d7bd82d1fbeefd3d7
zjun <565061763@qq.com>
2023-12-01 21:37:22 +0800

    demo

--- app/src/main/java/com/sd/demo/closeable/FileResource.kt
+++ app/src/main/java/com/sd/demo/closeable/FileResource.kt
@@ -2,24 +2,24 @@ package com.sd.demo.closeable
 
 import com.sd.lib.closeable.FCloseableFactory
 
+object FileResourceFactory {
+    private val _factory = FCloseableFactory(FileResource::class.java)
+
+    fun create(path: String): FileResource {
+        return _factory.create(path) { FileResourceImpl(path) }
+    }
+}
+
 interface FileResource : AutoCloseable {
     fun write(content: String)
 }
 
-class FileResourceImpl(private val path: String) : FileResource {
+private class FileResourceImpl(private val path: String) : FileResource {
     override fun write(content: String) {
         logMsg { "write $content $this" }
     }
 
     override fun close() {
         logMsg { "close $this" }
-    }
-}
-
-object FileResourceFactory {
-    private val _factory = FCloseableFactory(FileResource::class.java)
-
-    fun create(path: String): FileResource {
-        return _factory.create(path) { FileResourceImpl(path) }
     }
 }
