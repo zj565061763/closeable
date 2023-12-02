89f28ab9d28fa2ebbc294b6fb99fa15f22ff79d3
zjun <565061763@qq.com>
2023-12-02 18:01:11 +0800

    1.0.0-beta04

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "closeable"
-val libVersionName = "1.0.0-beta03"
+val libVersionName = "1.0.0-beta04"
 
 android {
     namespace = "com.sd.lib.closeable"
