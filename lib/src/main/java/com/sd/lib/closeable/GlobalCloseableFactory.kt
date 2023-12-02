97289796c4506b91abc2687629497f96cdfa952f
zjun <565061763@qq.com>
2023-12-02 17:52:13 +0800

    FGlobalCloseableFactory

--- lib/src/main/java/com/sd/lib/closeable/GlobalCloseableFactory.kt
+++ lib/src/main/java/com/sd/lib/closeable/GlobalCloseableFactory.kt
@@ -3,9 +3,13 @@ package com.sd.lib.closeable
 object FGlobalCloseableFactory {
     private val _holder: MutableMap<Class<out AutoCloseable>, FCloseableFactory<out AutoCloseable>> = hashMapOf()
 
-    @Suppress("UNCHECKED_CAST")
     @JvmStatic
-    fun <T : AutoCloseable> factoryOf(clazz: Class<T>): FCloseableFactory<T> {
+    fun <T : AutoCloseable> create(clazz: Class<T>, key: String, factory: () -> T): T {
+        return factoryOf(clazz).create(key, factory)
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun <T : AutoCloseable> factoryOf(clazz: Class<T>): FCloseableFactory<T> {
         synchronized(FGlobalCloseableFactory) {
             val value = _holder.getOrPut(clazz) {
                 object : FCloseableFactory<T>(clazz) {
@@ -20,8 +24,4 @@ object FGlobalCloseableFactory {
             return value as FCloseableFactory<T>
         }
     }
-}
-
-fun <T : AutoCloseable> fCloseableFactoryOf(clazz: Class<T>): FCloseableFactory<T> {
-    return FGlobalCloseableFactory.factoryOf(clazz)
 }
